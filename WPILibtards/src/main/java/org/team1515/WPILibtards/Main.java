976cd2c4a90549f8acbc18571b22477605b39245
HayleyIsCool <96212232+HayleyIsCool@users.noreply.github.com>
2023-04-10 16:22:50 -0700

    i did things

--- WPILibtards/src/main/java/org/team1515/WPILibtards/Main.java
+++ WPILibtards/src/main/java/org/team1515/WPILibtards/Main.java
@@ -2,12 +2,14 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package org.team1515.WPILibtards;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
 public final class Main {
-  private Main() {}
+  private Main() {
+    System.out.println("hi guys");
+  }
 
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
